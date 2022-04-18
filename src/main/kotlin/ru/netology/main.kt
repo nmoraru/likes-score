351ebc1565b36086c7a50562e88fe3cc42cfd0a5
Nikita Moraru <nomoraru@gmail.com>
2022-04-18 23:33:37 +0300

    code refactoring

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -2,6 +2,6 @@ package ru.netology
 
 fun main() {
     val likes: Int = 2291
-    val human: String = if (likes === 1 || (likes % 10 === 1 && likes != 11 && likes % 100 != 11) ) "человеку" else "людям"
+    val human: String = if (likes == 1 || (likes % 10 == 1 && likes != 11 && likes % 100 != 11) ) "человеку" else "людям"
     println("Понравилось $likes $human")
 }
